773b6682394c2df2ce50221df2a5286054bb4154
Bowen Zheng <bowenzheng2003@gmail.com>
2019-11-06 10:13:36 -0500

    removed euan

--- javascript.js
+++ javascript.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 const width = 30;
 const length = 30;
 
 //Test code
 var map = makeMap(width, length);
 document.getElementById("map").innerHTML = borderMap(map);
-=======
 const width= 30;
 const length= 30;
->>>>>>> 3e017fa10eccc882623f27ac2e353926808dda5f
