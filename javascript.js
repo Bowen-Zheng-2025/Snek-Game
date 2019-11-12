0b9d30662f3f1ac07dc870cc062347d403f27825
Euan Barclay <Silverswimmer63@gmail.com>
2019-11-12 09:28:39 -0500

    Made snake seeablerer on map

--- javascript.js
+++ javascript.js
@@ -4,6 +4,8 @@ const length = 30;
 var snake = [];
 //Test code
 var map = makeMap(width, length);
+ snake = placeSnake(map);
+ map = seeSnake(map, snake);
 var score = snake.length -2;
 document.getElementById("map").innerHTML = borderMap(map);
 document.getElementById("score").innerHTML = "score =" + score;
@@ -26,6 +28,10 @@ document.addEventListener('keydown', function(event) {
 document.addEventListener('keyup', function(event) {
   if (event.key == 'w') {
     document.getElementById("wKey").style.background = "#f0f0f2";
+    //Make the snake move
+    //add the new coordinates to the front of the snake array
+    //Remove the last item in the array
+    //Redraw the map
   }
   if (event.key == "a") {
     document.getElementById("aKey").style.background = "#f0f0f2";
